0121b6b78dd62218a8685d95b9e82c1b3ae6c63c
root <root@122.245.72.148.host.secureserver.net>
2023-04-12 20:53:58 +0000

    solve error

--- src/notAuth.js
+++ src/notAuth.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const NotAuth = () => {
   const user = localStorage.getItem("Access Token");
   if (user) {
-    return <Navigate to="/settings" />;
+    return <Navigate to="/room" />;
   } else {
     return <Navigate to="/login" />;
   }
